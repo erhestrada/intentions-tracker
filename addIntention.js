10d2dd395bb9193584272ffeba2c05b90a979673
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-21 12:36:38 -0400

    implement addIntentionToLocalStorage, use in addIntention when form submitted

--- addIntention.js
+++ addIntention.js
@@ -2,20 +2,22 @@ import { displayIntention } from "./displayIntentions";
 
 export function addIntention() {
   const intention = document.getElementById('add-intention-input').value.trim();
-  //addIntentionToLocalStorage(intention);
+  addIntentionToLocalStorage(intention);
   
   displayIntention(intention);
   console.log(intention);
 }
 
-
-
 function addIntentionToLocalStorage(intention) {
-
+    let intentions = loadIntentionsFromLocalStorage();
+    intentions.push(intention);
+    const jsonIntentions = JSON.stringify(intentions);
+    localStorage.setItem('intentions', jsonIntentions);
+    return intentions;
 }
 
 export function loadIntentionsFromLocalStorage() {
     const jsonIntentions = localStorage?.getItem('intentions') ?? JSON.stringify(['placeholder intention']);
-    const intentionsArray = JSON.parse(jsonIntentions);
-    return intentionsArray;
+    const intentions = JSON.parse(jsonIntentions);
+    return intentions;
 }
