c698552d46f093c6d127310db7600a641e0f28a7
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-31 17:42:48 -0400

    add times per day input

--- initializeIntentionsEntry.js
+++ initializeIntentionsEntry.js
@@ -1,3 +1,7 @@
+// get current date
+// i need to load my intentions log, get the most recent date
+// initialize intentions entry for each day between last date and current date
+
 export function initializeIntentionsEntry() {
 
 }
