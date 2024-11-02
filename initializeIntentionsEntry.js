78c2d76aa09cd0fe932421d61083c661ead315a3
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-01 20:28:11 -0400

    remove unused file

--- initializeIntentionsEntry.js
+++
@@ -1,7 +0,0 @@
-// get current date
-// i need to load my intentions log, get the most recent date
-// initialize intentions entry for each day between last date and current date
-
-export function initializeIntentionsEntry() {
-
-}
