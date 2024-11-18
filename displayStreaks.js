5d8889092af37e7cacd13475d372e151360f9b17
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-18 07:35:38 -0500

    delete displayStreaks.js

--- displayStreaks.js
+++
@@ -1,7 +0,0 @@
-import { calculateStreaks } from "./calculateStreaks";
-
-export function displayStreaks() {
-    const statesOfCheckboxes = JSON.parse(localStorage.getItem('statesOfCheckboxes')) || {};
-    const streaks = calculateStreaks(statesOfCheckboxes);
-    console.log('streaks', streaks);
-}
