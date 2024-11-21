a2cf97e31dcbe3570aa853b6e9f939797eb1cfb5
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-21 16:22:29 -0500

    change endpoint to match backend and return data

--- frontend/retrieveRequiredRepetitionsPerIntention.js
+++ frontend/retrieveRequiredRepetitionsPerIntention.js
@@ -1,9 +1,9 @@
 // Fetch and display data from SQLite database
 export async function retrieveRequiredRepetitionsPerIntention() {
     try {
-      const response = await fetch('http://192.168.86.195:3000/retrieve');
+      const response = await fetch('http://192.168.86.195:3000/retrieveRequiredRepetitionsPerIntention');
       const data = await response.json();
-      dataDiv.innerHTML = data.map(item => `<p>Stored Value: ${item.value}</p>`).join('');
+      return data;
     } catch (error) {
       console.error('Error retrieving data:', error);
     }
