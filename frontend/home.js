bc6c3290cd7e172e976aa95034378ced8ef59fde
Erik <180784081+erhetrada@users.noreply.github.com>
2024-11-22 14:16:37 -0500

    adjust css so boxes are centered even when there's a smaller number

--- frontend/home.js
+++ frontend/home.js
@@ -16,5 +16,6 @@ function displayIntentionBoxes(requiredRepetitionsPerIntention) {
 
 const requiredRepetitionsPerIntention = await retrieveRequiredRepetitionsPerIntention();
 //const requiredRepetitionsPerIntention = [{id: 1, intention: 'x', repetitions: 1}]
+//const requiredRepetitionsPerIntention = [{id: 1, intention: 'x', repetitions: 1}, {id: 1, intention: 'x', repetitions: 1}]
 console.log(requiredRepetitionsPerIntention);
 displayIntentionBoxes(requiredRepetitionsPerIntention);
