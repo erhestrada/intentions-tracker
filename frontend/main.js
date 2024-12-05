7604f30e67af297cfa5973711c0318eb963e28ec
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-05 03:49:58 -0500

    initialize getUserData()

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 import { typeIntentions, handleKeydown } from "./typeIntentions";
 import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
+import { getUserData } from "./getUserData";
 
 const userId = getOrCreateUniqueId();
 const intentions = Object.keys(JSON.parse(localStorage.getItem('requiredRepetitionsPerIntention')) || {});
