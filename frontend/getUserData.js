2aa11f41f9b89e6c81f31ab24e867f228a745681
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-06 19:02:36 -0500

    initialize getUserData with placeholder code for retrieving requiredRepetitionsPerIntention

--- frontend/getUserData.js
+++ frontend/getUserData.js
@@ -1,3 +1,9 @@
-export function getUserData() {
-    
+export async function getUserData() {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/retrieveRequiredRepetitionsPerIntention');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error retrieving data:', error);
+      }
 }
