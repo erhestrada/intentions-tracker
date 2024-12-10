6811597d473c8127f85e0aae1fedc0fd01f3f91a
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-10 07:53:41 -0500

    remove getUserData.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,7 @@
 import { typeIntentions, handleKeydown } from "./typeIntentions";
 import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
-import { getUserData } from "./getUserData";
 
 const userId = getOrCreateUniqueId();
-const userData = getUserData(userId);
 const intentions = Object.keys(JSON.parse(localStorage.getItem('requiredRepetitionsPerIntention')) || {});
 console.log(intentions);
 document.addEventListener('keydown', (e) => handleKeydown(e, intentions));
