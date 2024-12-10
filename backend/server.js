8b706efa8faf30e15878e054ae1f1f91a39043e4
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-10 03:51:49 -0500

    change order of table creation

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,14 @@ app.use(express.json());
 // Setup SQLite database
 const db = new sqlite3.Database('./data.db');
 
+// deprecating
 db.run('CREATE TABLE IF NOT EXISTS required_repetitions_per_intention (id INTEGER PRIMARY KEY, intention TEXT, repetitions INTEGER)');
+
 db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL)');
+db.run('CREATE TABLE IF NOT EXISTS intentions_log (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, date TEXT, intention TEXT, timestamp TEXT)');
 db.run('CREATE TABLE IF NOT EXISTS required_repetitions_per_intention_new (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, intention TEXT, repetitions INTEGER)');
 db.run('CREATE TABLE IF NOT EXISTS achievement_statuses (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, date TEXT, action TEXT, achievement_status INTEGER)');
 db.run('CREATE TABLE IF NOT EXISTS streaks (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, date TEXT, action TEXT, streak INTEGER)');
-db.run('CREATE TABLE IF NOT EXISTS intentions_log (id INTEGER PRIMARY KEY, uuid TEXT UNIQUE NOT NULL, date TEXT, intention TEXT, timestamp TEXT)');
 //db.run('DROP TABLE store_required_repetitions_per_intention');
 //db.run('DELETE FROM required_repetitions_per_intention'); // Deletes all rows
 
