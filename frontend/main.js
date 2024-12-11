5bbccb10f5e6d7a7b809208c322c527e77e96a7d
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-11 09:38:41 -0500

    load intentions from database

--- frontend/main.js
+++ frontend/main.js
@@ -3,9 +3,9 @@ import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
 import { retrieveRequiredRepetitionsPerIntention } from "./retrieveRequiredRepetitionsPerIntention";
 
 const userId = getOrCreateUniqueId();
-const intentions = Object.keys(JSON.parse(localStorage.getItem('requiredRepetitionsPerIntention')) || {});
-const intentions2 = await retrieveRequiredRepetitionsPerIntention();
-console.log('intention2', intentions2);
+
+// i should just be getting the column using sql
+const intentions = (await retrieveRequiredRepetitionsPerIntention()).map(obj => obj.intention);
 console.log('intentions', intentions);
 document.addEventListener('keydown', (e) => handleKeydown(e, intentions));
 typeIntentions(intentions);
