41a85900a591d7bbad3dc4468bfdce60d05ee088
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-11 09:03:25 -0500

    load required_repetitions_per_intention from database

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,11 @@
 import { typeIntentions, handleKeydown } from "./typeIntentions";
 import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
+import { retrieveRequiredRepetitionsPerIntention } from "./retrieveRequiredRepetitionsPerIntention";
 
 const userId = getOrCreateUniqueId();
 const intentions = Object.keys(JSON.parse(localStorage.getItem('requiredRepetitionsPerIntention')) || {});
-console.log(intentions);
+const intentions2 = await retrieveRequiredRepetitionsPerIntention();
+console.log('intention2', intentions2);
+console.log('intentions', intentions);
 document.addEventListener('keydown', (e) => handleKeydown(e, intentions));
 typeIntentions(intentions);
