34570156b697163213edab151c12e5800ebf29df
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-11 10:03:26 -0500

    give retrieveRequiredRepetitionsPerIntention uuid argument

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,8 @@ import { typeIntentions, handleKeydown } from "./typeIntentions";
 import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
 import { retrieveRequiredRepetitionsPerIntention } from "./retrieveRequiredRepetitionsPerIntention";
 
-const userId = getOrCreateUniqueId();
-
-// i should just be getting the column using sql
-const intentions = (await retrieveRequiredRepetitionsPerIntention()).map(obj => obj.intention);
+const uuid = getOrCreateUniqueId();
+const intentions = (await retrieveRequiredRepetitionsPerIntention(uuid)).map(obj => obj.intention);
 console.log('intentions', intentions);
 document.addEventListener('keydown', (e) => handleKeydown(e, intentions));
 typeIntentions(intentions);
