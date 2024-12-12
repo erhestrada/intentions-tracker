a44285b98cd1b4384215540bb9c581e6d09f49bd
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-12 18:14:34 -0500

    convert true/false achievementStatus into 1/0

--- frontend/storeAchievementStatus.js
+++ frontend/storeAchievementStatus.js
@@ -1,6 +1,7 @@
 
 //const uuid = '59a75576-4ef2-48b4-9aa9-89d44bfc00db';
 export async function storeAchievementStatus(uuid, date, intention, achievementStatus) {
+    achievementStatus = achievementStatus ? 1 : 0;
     try {
       const response = await fetch('http://192.168.86.195:3000/storeAchievementStatus', {
         method: 'POST',
