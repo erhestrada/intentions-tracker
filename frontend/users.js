f8ba32845afe03b6f636a1c7d557e8c73e800cc7
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-13 17:57:34 -0500

    implement tentative user information display

--- frontend/users.js
+++ frontend/users.js
@@ -1,4 +1,12 @@
 import { retrieveUsers } from "./retrieveUsers";
+import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
+import { retrieveAndFormatRequiredRepetitionsPerIntention } from "./retrieveRequiredRepetitionsPerIntention";
+import { typeIntentions, handleKeydown } from "./typeIntentions";
+import { retrieveAchievementStatuses } from "./retrieveAchievementStatuses";
+import { retrieveAndFormatIntentionsLog } from "./retrieveIntentionsLog";
+import { retrieveStreaks } from "./retrieveStreaks";
+import { storeAchievementStatus } from "./storeAchievementStatus";
+import { storeStreak } from "./storeStreak";
 
 export async function displayInformationForUsers() {
     const users = (await retrieveUsers()).map(obj => obj.uuid);
@@ -7,6 +15,197 @@ export async function displayInformationForUsers() {
 
 export async function displayInformationForUser(uuid) {
     console.log(uuid);
+    const requiredRepetitionsPerIntention = await retrieveAndFormatRequiredRepetitionsPerIntention(uuid);
+    console.log('rrpi', requiredRepetitionsPerIntention);
+    const intentionsLog = await retrieveAndFormatIntentionsLog(uuid);
+    const intentionsRepetitionsPerDate = makeIntentionsRepetitionsPerDateFromIntentionsLog(intentionsLog);
+
+    displayIntentionBoxes(uuid, requiredRepetitionsPerIntention, intentionsRepetitionsPerDate);
 }
+
+
+async function displayIntentionBoxes(uuid, requiredRepetitionsPerIntention, intentionsRepetitionsPerDate) {
+    //let achievementStatuses = JSON.parse(localStorage.getItem('achievementStatuses')) || {};
+    let achievementStatuses = await retrieveAchievementStatuses(uuid);
+    console.log('1', achievementStatuses);
+    //console.log('2', achievementStatuses2);
+
+    let streaks = await retrieveStreaks(uuid);
+    //let streaks = JSON.parse(localStorage.getItem('streaks')) || {};
+    console.log('streaks initial', streaks);
+
+    const intentionBoxesContainer = document.getElementById('intention-boxes-container');
+
+    const date = (new Date()).toLocaleDateString();
+    const yesterdaysDate = getYesterdaysDate(date);
+
+    for (const [intention, repetitions] of Object.entries(requiredRepetitionsPerIntention)) {
+        const intentionBox = document.createElement('div');
+        intentionBox.className = 'intention-box';
+        intentionBox.id = intention;
+
+        const intentionTextElement = document.createElement('p');
+        intentionTextElement.innerText = intention;
+        
+        const requiredRepetitionsTextElement = document.createElement('p');
+        let repetitionsOnDate;
+        if (date in intentionsRepetitionsPerDate) {
+            repetitionsOnDate = intentionsRepetitionsPerDate[date][intention] || 0;
+        } else {
+            repetitionsOnDate = 0;
+        }
+        requiredRepetitionsTextElement.innerText = repetitionsOnDate + '/' + repetitions + ' repetitions';
+
+        const repetitionSquaresElement = document.createElement('p');
+        //repetitionSquaresElement.innerText = '⬜'.repeat(repetitions);
+        repetitionSquaresElement.innerText = displayProgress(intention, repetitions, intentionsRepetitionsPerDate);
+
+        const successTextElement = document.createElement('p');
+        successTextElement.innerText = 'Achievement Status';
+
+        const successButton = document.createElement('button');
+        successButton.innerText = '✔️';
+
+        const streakElement = document.createElement('p');
+        let streaksValue = streaks?.[date]?.[intention] ?? streaks?.[yesterdaysDate]?.[intention] ?? 0;
+        streakElement.innerText = "Streak: " + streaksValue;
+
+        intentionBoxesContainer.appendChild(intentionBox);
+        intentionBox.appendChild(intentionTextElement);
+        intentionBox.appendChild(requiredRepetitionsTextElement);
+        intentionBox.appendChild(repetitionSquaresElement);
+        intentionBox.appendChild(successTextElement);
+        intentionBox.appendChild(streakElement);
+    }
+}
+
+function displayProgress(intention, requiredRepetitions, intentionsRepetitionsPerDate) {
+    const requirementSymbol = '⬜';
+    const repetitionSymbol = '✅';
+    /*
+    let date = (new Date()).toLocaleDateString('en-US', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit'
+      });
+    */
+    const date = (new Date()).toLocaleDateString();
+    let repetitionsOnDate;
+    if (date in intentionsRepetitionsPerDate) {
+        repetitionsOnDate = intentionsRepetitionsPerDate[date][intention] || 0;
+    } else {
+        repetitionsOnDate = 0;
+    }
+
+    let repetitionsLeftToDo = requiredRepetitions - repetitionsOnDate;
+    if (repetitionsLeftToDo < 0) {
+        repetitionsLeftToDo = 0;
+    }
+
+    // display all checkmarks
+    const innerText = intention + ' ' + repetitionSymbol.repeat(repetitionsOnDate) + requirementSymbol.repeat(repetitionsLeftToDo);
+    return innerText;
+}
+
+function makeIntentionsRepetitionsPerDateFromIntentionsLog(intentionsLog) {
+    const intentionsRepetitionsPerDate = {};
+    for (const [date, intentionsAndDateTimes] of Object.entries(intentionsLog)) {
+        // just store intentionsExpressedOnDate in main.js in the first place
+        const intentionsExpressedOnDate = intentionsAndDateTimes.filter(intentionAndDateTime => {
+            const [intention, isoDateTime] = intentionAndDateTime;
+            const abc = new Date(isoDateTime);
+            if (abc.toLocaleDateString() === date) {
+                return true;
+            } 
+        }).map(element => element[0]);
+        
+        const intentionsRepetitionsOnDate = intentionsExpressedOnDate.reduce((acc, element) => {
+            acc[element] = (acc[element] || 0) + 1;
+            return acc;
+        }, {});
+
+        intentionsRepetitionsPerDate[date] = intentionsRepetitionsOnDate;
+    }
+    return intentionsRepetitionsPerDate;
+}
+
+function updateAchievementStatuses(uuid, achievementStatuses, date, intention, achievementStatus) {
+    // Ensure the date is part of the object, creating it if necessary
+    if (!achievementStatuses[date]) {
+        achievementStatuses[date] = {};
+    }
+    achievementStatuses[date][intention] = achievementStatus;
+    storeAchievementStatus(uuid, date, intention, achievementStatus);
+    
+    //localStorage.setItem('achievementStatuses', JSON.stringify(achievementStatuses));
+    return achievementStatuses
+}
+
+function updateStreaks(uuid, streaks, date, intention, achievementStatuses) {
+    const yesterdaysDate = getYesterdaysDate(date);
+    const achievementStatus = achievementStatuses[date][intention];
+
+    // if yesterday's date not in streaks, today's streak for the intention is at 0
+    if (!streaks[yesterdaysDate]) {
+        // if streaks[date] already initialized as an object, set streaks[date][intention] to 0
+        if (date in streaks) {
+            streaks[date][intention] = 0;
+        } else {
+            // initialize streaks[date] as an object with key intention and value 0
+            streaks[date] = {[intention]: 0};
+        }
+    }
+
+    // placeholder streak value of false
+    if (!(date in streaks)) {
+        streaks[date] = {[intention]: false};
+    } else {
+        if (!(intention in streaks[date])) {
+            streaks[date][intention] = false;
+        }
+    }
+
+    //let streakValue = streaks?.[date]?.[intention] ?? streaks?.[yesterdaysDate]?.[intention] ?? 0;
+    let streak;
+    let yesterdaysStreakValue = streaks?.[yesterdaysDate]?.[intention] ?? 0;
+
+    if (achievementStatus === true) {
+        streak = yesterdaysStreakValue + 1
+        streaks[date][intention] = streak;
+    } else {
+        // streak resets to 0 if failed
+        streak = 0;
+        streaks[date][intention] = streak;
+    }
+    
+    storeStreak(uuid, date, intention, streak);
+    //localStorage.setItem('streaks', JSON.stringify(streaks));
+    console.log('streaks', streaks);
+    return streaks;
+}
+
+function undoStreakUpdate(uuid, streaks, date, intention) {
+    const yesterdaysDate = getYesterdaysDate(date);
+    streaks[date][intention] = streaks?.[yesterdaysDate]?.[intention] ?? 0;
+    const streak = streaks?.[yesterdaysDate]?.[intention] ?? 0;
+    storeStreak(uuid, date, intention, streak);
+    //localStorage.setItem('streaks', JSON.stringify(streaks));
+    return streaks
+}
+
+function getYesterdaysDate(dateStr) {
+    // Create a Date object from the given date string (MM/DD/YYYY)
+    const dateParts = dateStr.split('/');
+    const date = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]); // Months are 0-based in JavaScript
+    
+    // Subtract one day to get yesterday's date
+    date.setDate(date.getDate() - 1);
+    
+    // Format the new date as MM/DD/YYYY
+    const yesterday = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    
+    return yesterday;
+}
+
 
 displayInformationForUsers();
