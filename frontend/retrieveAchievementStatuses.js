99ca23e3ea929c55b8c617ea9f35068322115804
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-15 16:11:54 -0500

    implement retrieveAndFormatAchievementStatuses, makeAchievementStatusesFromRows

--- frontend/retrieveAchievementStatuses.js
+++ frontend/retrieveAchievementStatuses.js
@@ -1,3 +1,9 @@
+export async function retrieveAndFormatAchievementStatuses(uuid) {
+  const rows = await retrieveAchievementStatuses(uuid);
+  const achievementStatuses = makeAchievementStatusesFromRows(rows);
+  return achievementStatuses;
+}
+
 export async function retrieveAchievementStatuses(uuid) {
     try {
       //const response = await fetch('http://192.168.86.195:3000/retrieveRequiredRepetitionsPerIntention');
@@ -7,4 +13,22 @@ export async function retrieveAchievementStatuses(uuid) {
     } catch (error) {
       console.error('Error retrieving data:', error);
     }
+}
+
+// date, action, achievement_status
+function makeAchievementStatusesFromRows(rows) {
+  const achievementStatuses = rows.reduce((accumulator, row) => {
+    const date = row['date'];
+    const action = row['action'];
+    const achievementStatus = row['achievement_status'];
+
+    if (!(date in accumulator)) {
+      accumulator[date] = {action: achievementStatus};
+    } else {
+      accumulator[date][action] = achievementStatus;
+    }
+
+    return accumulator;
+  }, {});
+  return achievementStatuses;
 }
