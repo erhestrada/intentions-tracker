ae72c7acf0f5ca40732c9ed72b076dffd6290a93
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-17 14:25:58 -0500

    implement sendChatMessage

--- frontend/sendChatMessage.js
+++ frontend/sendChatMessage.js
@@ -1,24 +1,11 @@
 import { retrieveChatHistory } from "./retrieveChatHistory";
+import { storeChatMessage } from "./storeChatMessage";
+import { displayChatHistory } from "./displayChatHistory";
 
 export async function sendChatMessage(uuid) {
-    const chatHistory = await retrieveChatHistory();
-    //displayChatHistory(chatHistory);
-
     const chatMessage = document.getElementById('send-chat-message-input').value;
-    console.log(chatMessage);
-    
-}
+    await storeChatMessage(uuid, chatMessage);
 
-
-export function addIntention(uuid) {
-  //let requiredRepetitionsPerIntention = JSON.parse(localStorage.getItem('requiredRepetitionsPerIntention')) || {};
-  let requiredRepetitionsPerIntention = retrieveRequiredRepetitionsPerIntention(uuid);
-  const intention = document.getElementById('add-intention-input').value.trim();
-  const requiredRepetitions = document.getElementById('required-repetitions-input').value;
-  requiredRepetitionsPerIntention[intention] = requiredRepetitions;
-  //localStorage.setItem('requiredRepetitionsPerIntention', JSON.stringify(requiredRepetitionsPerIntention));
-  storeRequiredRepetitionsForIntention(uuid, intention, requiredRepetitions);
-
-  displaySquares({[intention]: requiredRepetitions});
-  document.getElementById('add-intention-input').value = ''; // clear input when intention added
+    const chatHistory = await retrieveChatHistory();
+    displayChatHistory(chatHistory);
 }
