34bbf43cdfa299d5ff249127e67eecdbc2bec10f
Erik <180784081+erhetrada@users.noreply.github.com>
2024-12-17 14:26:56 -0500

    clear on send

--- frontend/sendChatMessage.js
+++ frontend/sendChatMessage.js
@@ -5,6 +5,7 @@ import { displayChatHistory } from "./displayChatHistory";
 export async function sendChatMessage(uuid) {
     const chatMessage = document.getElementById('send-chat-message-input').value;
     await storeChatMessage(uuid, chatMessage);
+    document.getElementById('send-chat-message-input').value = ''; // clear input when intention added
 
     const chatHistory = await retrieveChatHistory();
     displayChatHistory(chatHistory);
