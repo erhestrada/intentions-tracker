e41841fdedee00610284221af49a640c85dc9e37
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-21 15:53:13 -0500

    remove main.js

--- frontend/main.js
+++
@@ -1,9 +0,0 @@
-import { typeIntentions, handleKeydown } from "./typeIntentions";
-import { getOrCreateUniqueId } from "./getOrCreateUniqueUserId";
-import { retrieveRequiredRepetitionsPerIntention } from "./retrieveRequiredRepetitionsPerIntention";
-
-const uuid = getOrCreateUniqueId();
-const intentions = (await retrieveRequiredRepetitionsPerIntention(uuid)).map(obj => obj.intention);
-console.log('intentions', intentions);
-document.addEventListener('keydown', (e) => handleKeydown(e, intentions));
-typeIntentions(intentions);
