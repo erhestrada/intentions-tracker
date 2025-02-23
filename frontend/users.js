10f936712d81019d05d3f69b018a3563a59702f8
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-22 22:31:37 -0500

    display uuid for each user

--- frontend/users.js
+++ frontend/users.js
@@ -1,11 +1,17 @@
 import { retrieveUsers } from "./retrieveUsers"
 
-function getAndDisplayAllUsers() {
+async function getAndDisplayAllUsers() {
+    const users = await retrieveUsers();
+    console.log(users);
 
-}
+    const parentElement = document.getElementById('users-container');
 
-function getAllUsers() {
+    users.forEach((userData) => {
+        const userElement = document.createElement('p');
+        userElement.innerText = userData.uuid;
 
+        parentElement.appendChild(userElement);
+    });
 }
 
 getAndDisplayAllUsers();
