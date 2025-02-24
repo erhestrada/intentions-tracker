c0e812fa113b568ff12fe73f5f7e110a6c5b9c56
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-24 10:32:50 -0500

    change userElement from p to a

--- frontend/users.js
+++ frontend/users.js
@@ -2,12 +2,14 @@ import { retrieveUsers } from "./retrieveUsers"
 
 async function getAndDisplayAllUsers() {
     const users = await retrieveUsers();
-    console.log(users);
 
     const parentElement = document.getElementById('users-container');
 
     users.forEach((userData) => {
-        const userElement = document.createElement('p');
+        const userElement = document.createElement('a');
+        userElement.className = 'clickable';
+        userElement.style.display = 'block';
+        userElement.style.marginBottom = "10px";
         userElement.innerText = userData.uuid;
 
         userElement.addEventListener('click', () => {
