a54cb3f4673842daa44d4d6399460c7625eb7840
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-26 13:49:51 -0500

    implement sendBondRequest

--- frontend/sendBondRequest.js
+++ frontend/sendBondRequest.js
@@ -1,3 +1,22 @@
-export function sendBondRequest(senderId, bondedIntentions) {
+export async function sendBondRequest(senderId, bondedIntentions) {
+    const receiverIds = Object.keys(bondedIntentions);
+    for (const receiverId of receiverIds) {
+        const acceptanceStatus = senderId === receiverId ? 'accepted' : 'pending';
+        storeBondRequest(senderId, receiverId, bondedIntentions, acceptanceStatus);
+    }
+};
 
+async function storeBondRequest(senderId, receiverId, bondedIntentions) {
+    try {
+        const response = await fetch('http://192.168.86.195:3000/storeBondRequest', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ senderId, receiverId, bondedIntentions })
+        });
+          const result = await response.json();
+          console.log('Data Stored:', result);
+    
+      } catch (error) {
+          console.error('Error storing data:', error);
+      }
 }
