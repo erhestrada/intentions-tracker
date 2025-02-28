d4eab11eeaf861dc54dcde32e2ed3e8077faba1b
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-28 17:07:28 -0500

    implement setUsername api function

--- frontend/setupLogInButton.js
+++ frontend/setupLogInButton.js
@@ -1,5 +1,3 @@
-// need api function and api for setting username
-
 export function setupLogInButton() {
     const logInButton = document.getElementById('log-in-button');
 
@@ -10,7 +8,18 @@ export function setupLogInButton() {
     });
 }
 
-
+// need api function and api for setting username
 async function setUsername(username) {
+    try {
+        const response = fetch('http://192.168.86.195:3000/storeUsername', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: { username }
+        });
+        const result = await response.json();
+        console.log('Username Stored', result);
 
+    } catch(error) {
+        console.error("Error setting username: ", error);
+    }
 }
