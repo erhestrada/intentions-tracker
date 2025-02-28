644c75368d13dca2484dd56b330bb2ad21921d57
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-28 16:50:09 -0500

    get username and logInButton, initialize click event listener

--- frontend/setupLogInButton.js
+++ frontend/setupLogInButton.js
@@ -1,3 +1,8 @@
-function setupLogInButton() {
-    
+export function setupLogInButton() {
+    const username = document.getElementById('username-input').value.trim();
+    const logInButton = document.getElementById('log-in-button');
+
+    logInButton.addEventListener('click', () => {
+        console.log('log in clicked');
+    });
 }
