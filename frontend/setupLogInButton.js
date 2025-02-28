3b489bc85d5352ea88807db7d3fc0cd2f8db10e5
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-28 16:58:19 -0500

    intialize setUsername

--- frontend/setupLogInButton.js
+++ frontend/setupLogInButton.js
@@ -1,8 +1,16 @@
+// need api function and api for setting username
+
 export function setupLogInButton() {
     const logInButton = document.getElementById('log-in-button');
 
-    logInButton.addEventListener('click', () => {
+    logInButton.addEventListener('click', async () => {
         const username = document.getElementById('username-input').value.trim();
+        setUsername(username);
         console.log(username);
     });
+}
+
+
+async function setUsername(username) {
+
 }
