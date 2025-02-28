12207e0b746dec386507a0ed215c6e4d0acb1b26
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-28 13:41:23 -0500

    implement retrieveBondRequestsForUser

--- frontend/inbox.js
+++ frontend/inbox.js
@@ -1,3 +1,13 @@
 function retrieveAndDisplayBondRequestsForUser() {
-    
+
+}
+
+async function retrieveBondRequestsForUser(uuid) {
+    try {
+        const response = await fetch(`http://192.168.86.195:3000/retrieveBondRequestsForUser?uuid=${encodeURIComponent(uuid)}`);
+        const data = await response.json();
+        return data
+    } catch(error) {
+        console.error('Error retrieving data:', error);
+    }
 }
