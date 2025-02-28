3ffcd9d33ed36012a410a6d3463cbe518b5664c4
Erik <180784081+erhetrada@users.noreply.github.com>
2025-02-28 16:54:41 -0500

    get username value when logInButton clicked

--- frontend/setupLogInButton.js
+++ frontend/setupLogInButton.js
@@ -1,8 +1,8 @@
 export function setupLogInButton() {
-    const username = document.getElementById('username-input').value.trim();
     const logInButton = document.getElementById('log-in-button');
 
     logInButton.addEventListener('click', () => {
-        console.log('log in clicked');
+        const username = document.getElementById('username-input').value.trim();
+        console.log(username);
     });
 }
