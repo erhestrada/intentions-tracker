e18cd9c95b86e2b12348e64c991d018905cf8894
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-01 19:41:55 -0500

    run sql statement to insert uuid, username into usernames table

--- backend/server.js
+++ backend/server.js
@@ -114,10 +114,14 @@ app.get('/retrieveUsers', (req, res) => {
 // ---------------------
 
 app.post('/storeUsername', (req, res) => {
-  const { username } = req.body;
-  console.log(username);
-  db.run('INSERT INTO usernames (')
-  res.json({'username': 'username'});
+  const { uuid, username } = req.body;
+  db.run('INSERT INTO usernames (uuid, username) VALUES (?, ?', [uuid, username], function(err) {
+    if (err) {
+      console.log('Error storing bond request:', err.message);
+      return res.status(500).json({ error: 'Failed to store data', details: err.message });
+    }
+    res.json({'message': `username: ${username} stored successfully`});
+  });
 });
 
 
