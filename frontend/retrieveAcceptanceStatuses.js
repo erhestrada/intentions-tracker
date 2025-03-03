f22fdcff4a652a3b2b5bba346f3b8732f9ded5f9
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-03 18:05:01 -0500

    return data

--- frontend/retrieveAcceptanceStatuses.js
+++ frontend/retrieveAcceptanceStatuses.js
@@ -2,8 +2,7 @@ export async function retrieveAcceptanceStatuses(bondedIntentionsJson) {
     try {
         const response = await fetch(`http://192.168.86.195:3000/retrieveAcceptanceStatuses?uuid=${encodeURIComponent(bondedIntentionsJson)}`);
         const data = await response.json();
-        const username = data.username;
-        return username
+        return data
     } catch(error) {
         console.error("Error retrieving acceptance statuses", error);
     }
