3459304548c59e0b71d4051460ca1ddbd04c3a28
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-02 21:04:39 -0500

    retrieve and display usernames

--- frontend/users.js
+++ frontend/users.js
@@ -1,7 +1,15 @@
 import { retrieveUsers } from "./retrieveUsers"
+import { retrieveUsername } from "./storeAndRetrieveUsername";
 
 async function getAndDisplayAllUsers() {
     const users = await retrieveUsers();
+
+    const userObject = {};
+    
+    for (const userData of users) {
+        const username = await retrieveUsername(userData.uuid);
+        userObject[userData.uuid] = username; // Add the uuid as the key and username as the value
+    }
 
     const parentElement = document.getElementById('users-container');
 
@@ -11,7 +19,7 @@ async function getAndDisplayAllUsers() {
         userElement.style.display = 'block';
         userElement.style.marginBottom = "10px";
         userElement.href = `userProfile.html?uuid=${userData.uuid}`;
-        userElement.innerText = userData.uuid;
+        userElement.innerText = userObject[userData.uuid];
         parentElement.appendChild(userElement);
     });
 }
