03906cc9e10a0433d94d0f9142d48a03752f3bac
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-03 18:05:54 -0500

    change query paramter to bondedIntentionsJson

--- frontend/retrieveAcceptanceStatuses.js
+++ frontend/retrieveAcceptanceStatuses.js
@@ -1,6 +1,6 @@
 export async function retrieveAcceptanceStatuses(bondedIntentionsJson) {
     try {
-        const response = await fetch(`http://192.168.86.195:3000/retrieveAcceptanceStatuses?uuid=${encodeURIComponent(bondedIntentionsJson)}`);
+        const response = await fetch(`http://192.168.86.195:3000/retrieveAcceptanceStatuses?bondedIntentionsJson=${encodeURIComponent(bondedIntentionsJson)}`);
         const data = await response.json();
         return data
     } catch(error) {
