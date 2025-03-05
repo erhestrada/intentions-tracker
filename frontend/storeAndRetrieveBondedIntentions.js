922dffd0b1020f3078694debd16088165f002e62
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-05 16:34:05 -0500

    initialize storeBondedIntentions, retrieveBondedIntentions, updateStreakGivenBonds

---
+++ frontend/storeAndRetrieveBondedIntentions.js
@@ -0,0 +1,18 @@
+// table userId column, intention column, bonded_intentions column
+export async function storeBondedIntentions() {
+
+}
+
+export async function retrieveBondedIntentions() {
+
+}
+
+
+
+// input: intention
+// output: intention it's bonded to
+
+function updateStreakGivenBonds(intention, bondedIntentions) {
+    // only +1 streak if every achievementStatus is True, else reset to 0
+
+}
