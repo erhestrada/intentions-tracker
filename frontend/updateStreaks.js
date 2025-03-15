2ab041d1393e81590c1eb9db2eec6e4292195810
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-15 18:19:38 -0400

    rename resetStreaks to resetBrokenStreaks

--- frontend/updateStreaks.js
+++ frontend/updateStreaks.js
@@ -143,7 +143,7 @@ async function updateStreaksForBondedUsers(bondedIntentions, date, yesterdaysDat
 // only do this once per day the first time page is opened for each intention
 // if all bondedIntentions were not achieved yesterday, set streaks to 0 for those intentions
 
-export async function resetStreaks() {
+export async function resetBrokenStreaks() {
 
 }
 
