4c807cb8aceac339a6698a80b49ee240ebf09223
Erik <180784081+erhetrada@users.noreply.github.com>
2025-03-22 07:14:19 -0400

    make setUp function, click groups tab to start

--- frontend/groups2.js
+++ frontend/groups2.js
@@ -22,40 +22,41 @@ function openTab(event, tabName) {
   event.currentTarget.classList.add('active');
 }
   
-// Default open the first tab (Article)
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.tab-link').click();
-});
-
-const uuid = getOrCreateUniqueId();
-const groupsForUser = await retrieveGroupsForUser(uuid);
-const groups = await retrieveGroups();
-
-console.log('hello');
-const myGroupsContainer = document.getElementById('my-groups-container');
-
-const groupElement = document.createElement('p');
-groupElement.innerText = groupsForUser[0].group_name;
-
-myGroupsContainer.appendChild(groupElement);
-
-const searchGroupsContainer = document.getElementById('search-groups-container');
-
-const searchGroupsElement = document.createElement('p');
-searchGroupsElement.innerText = groups[0].group_name;
-
-searchGroupsContainer.appendChild(searchGroupsElement);
+async function setUp() {
+  const uuid = getOrCreateUniqueId();
+  const groupsForUser = await retrieveGroupsForUser(uuid);
+  const groups = await retrieveGroups();
+  
+  console.log('hello');
+  const myGroupsContainer = document.getElementById('my-groups-container');
+  
+  const groupElement = document.createElement('p');
+  groupElement.innerText = groupsForUser[0].group_name;
+  
+  myGroupsContainer.appendChild(groupElement);
+  
+  const searchGroupsContainer = document.getElementById('search-groups-container');
+  
+  const searchGroupsElement = document.createElement('p');
+  searchGroupsElement.innerText = groups[0].group_name;
+  
+  searchGroupsContainer.appendChild(searchGroupsElement);
+}
 
 const myGroupsTab = document.getElementById('my-groups-tab');
 const searchGroupsTab = document.getElementById('search-groups-tab');
 
 myGroupsTab.addEventListener('click', (event) => {
   openTab(event, 'my-groups-container')
-  console.log(groupsForUser);
+  //console.log(groupsForUser);
   
 });
 
 searchGroupsTab.addEventListener('click', (event) => {
   openTab(event, 'search-groups-container')
-  console.log(groups);
+  //console.log(groups);
 });
+
+setUp();
+
+myGroupsTab.click();
