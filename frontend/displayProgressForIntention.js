74a8c305244625d59a49cf4aa21ec257af4eecc4
Erik <180784081+erhetrada@users.noreply.github.com>
2025-04-02 13:55:28 -0400

    implement placeholder content for progress container

--- frontend/displayProgressForIntention.js
+++ frontend/displayProgressForIntention.js
@@ -1,3 +1,10 @@
 export function displayProgressForIntention(containerId) {
+    const container = document.getElementById(containerId);
 
+    container.innerHTML = '';  // or container.textContent = '';
+
+    const element = document.createElement('p');
+    element.innerText = 'placeholder';
+
+    container.appendChild(element);
 }
