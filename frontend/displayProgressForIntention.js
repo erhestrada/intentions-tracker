58098cedd1444826c2e2f76f78421298492f456f
Erik <180784081+erhetrada@users.noreply.github.com>
2025-04-02 15:50:31 -0400

    implement getAchievementStatusPerDateForIntention

--- frontend/displayProgressForIntention.js
+++ frontend/displayProgressForIntention.js
@@ -5,6 +5,7 @@ export function displayProgressForIntention(containerId, intention, formattedAch
     container.innerHTML = '';  // or container.textContent = '';
 
     const achievementStatusPerDate = getAchievementStatusPerDateForIntention(intention, formattedAchievementStatuses)
+    console.log(achievementStatusPerDate);
 
     const element = document.createElement('p');
     element.innerText = intention;
@@ -13,5 +14,11 @@ export function displayProgressForIntention(containerId, intention, formattedAch
 }
 
 function getAchievementStatusPerDateForIntention(intention, formattedAchievementStatuses) {
-
+    let intentionAchievementStatusPerDate = {};
+    for (const [date, dateAchievementStatuses] of Object.entries(formattedAchievementStatuses)) {
+        if (dateAchievementStatuses.hasOwnProperty(intention)) {
+            intentionAchievementStatusPerDate[date] = dateAchievementStatuses[intention];
+        }
+    }
+    return intentionAchievementStatusPerDate;
 }
